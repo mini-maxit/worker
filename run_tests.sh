58faad1c551e1c6624e9cd847a2d04ae3d0a9691
Matios102 <matios102@gmail.com>
2025-03-06 10:43:04 +0100

    dcoker for testsing

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,6 @@ docker compose -f docker-compose.test.yml up --build -d
 
 sleep 15
 
-docker compose exec go_app go test -count=1 -timeout 60s -v ./tests
+docker compose exec go_app go test -count=1 -timeout 100s -v ./tests
 
 docker compose down
