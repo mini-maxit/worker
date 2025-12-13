537718fae729f641935388075816d6beaf475f7f
Matios102 <matios102@gmail.com>
2025-12-13 22:14:19 +0100

    feat: refactor executor methods and add container name sanitization

--- internal/stages/executor/executor.go
+++ internal/stages/executor/executor.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/docker/docker/api/types/container"
@@ -63,7 +64,7 @@ func (d *executor) ExecuteCommand(
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(d.maxRunTimeSec)*time.Second)
 	defer cancel()
 
-	env := d.buildEnvironmentVariables(cfg)
+	env := buildEnvironmentVariables(cfg)
 
 	dockerImage, err := cfg.LanguageType.GetDockerImage(cfg.LanguageVersion)
 	if err != nil {
@@ -72,20 +73,21 @@ func (d *executor) ExecuteCommand(
 		return err
 	}
 
-	containerCfg := d.buildContainerConfig(
+	containerCfg := buildContainerConfig(
 		cfg.DirConfig.PackageDirPath,
 		dockerImage,
 		cfg.DirConfig.UserExecFilePath,
 		env,
 	)
 
-	hostCfg := d.buildHostConfig()
+	hostCfg := buildHostConfig()
 
 	if err := d.docker.EnsureImage(ctx, dockerImage); err != nil {
 		return err
 	}
 
-	containerID, err := d.docker.CreateContainer(ctx, containerCfg, hostCfg, cfg.MessageID)
+	containerName := SanitizeContainerName(cfg.MessageID)
+	containerID, err := d.docker.CreateContainer(ctx, containerCfg, hostCfg, containerName)
 	if err != nil {
 		return err
 	}
@@ -136,7 +138,36 @@ func (d *executor) ExecuteCommand(
 	return nil
 }
 
-func (d *executor) buildEnvironmentVariables(cfg CommandConfig) []string {
+func (d *executor) waitForContainer(
+	ctx context.Context, containerID string,
+) error {
+	timeout := time.Duration(d.maxRunTimeSec) * time.Second
+	exitCode, err := d.docker.WaitContainer(ctx, containerID, timeout)
+	if err != nil {
+		if errors.Is(err, customErr.ErrContainerTimeout) {
+			d.docker.ContainerKill(ctx, containerID, "SIGKILL")
+			return customErr.ErrContainerTimeout
+		}
+		return err
+	}
+
+	if exitCode != 0 {
+		return customErr.ErrContainerFailed
+	}
+
+	return nil
+}
+
+func SanitizeContainerName(raw string) string {
+	re := regexp.MustCompile("[^a-zA-Z0-9_.-]")
+	cleaned := re.ReplaceAllString(raw, "-")
+	if cleaned == "" {
+		cleaned = "untitled"
+	}
+	return "submission-" + cleaned
+}
+
+func buildEnvironmentVariables(cfg CommandConfig) []string {
 	timeEnv := make([]string, len(cfg.TestCases))
 	memEnv := make([]string, len(cfg.TestCases))
 	for i, tc := range cfg.TestCases {
@@ -178,7 +209,7 @@ func (d *executor) buildEnvironmentVariables(cfg CommandConfig) []string {
 	}
 }
 
-func (d *executor) buildContainerConfig(
+func buildContainerConfig(
 	userPackageDirPath string,
 	dockerImage string,
 	absoluteSolutionPath string,
@@ -199,7 +230,7 @@ func (d *executor) buildContainerConfig(
 	}
 }
 
-func (d *executor) buildHostConfig() *container.HostConfig {
+func buildHostConfig() *container.HostConfig {
 	return &container.HostConfig{
 		AutoRemove:  false,
 		NetworkMode: container.NetworkMode("none"),
@@ -212,24 +243,4 @@ func (d *executor) buildHostConfig() *container.HostConfig {
 		CgroupnsMode: container.CgroupnsModePrivate,
 		IpcMode:      container.IpcMode("private"),
 	}
-}
-
-func (d *executor) waitForContainer(
-	ctx context.Context, containerID string,
-) error {
-	timeout := time.Duration(d.maxRunTimeSec) * time.Second
-	exitCode, err := d.docker.WaitContainer(ctx, containerID, timeout)
-	if err != nil {
-		if errors.Is(err, customErr.ErrContainerTimeout) {
-			d.docker.ContainerKill(ctx, containerID, "SIGKILL")
-			return customErr.ErrContainerTimeout
-		}
-		return err
-	}
-
-	if exitCode != 0 {
-		return customErr.ErrContainerFailed
-	}
-
-	return nil
 }
