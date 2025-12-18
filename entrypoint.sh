697823511d62694e85e5731fcc12413b1dcc25e3
Matios102 <matios102@gmail.com>
2025-12-18 16:21:57 +0100

    feat: add entrypoint script for runtime image management

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+RUNTIME_IMAGE_BASE="ghcr.io/mini-maxit/runtime"
+
+RUNTIME_LANG_VERSIONS=(
+    "cpp:latest"
+)
+
+for lang_version in "${RUNTIME_LANG_VERSIONS[@]}"; do
+    image="${RUNTIME_IMAGE_BASE}-${lang_version}"
+    echo "Pulling $image..."
+    if docker pull "$image"; then
+        echo "Successfully pulled $image"
+    else
+        echo "Warning: Failed to pull $image, will be pulled on-demand"
+    fi
+done
+
+exec /app/worker-service "$@"
